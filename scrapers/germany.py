ca978c48cd252e8a2525c71dba84b10b99592da9
Helina Tefera <helinatefera1212@gmail.com>
2025-02-01 21:11:39 +0300

    (fix) Remove unnecessary blank lines and update lambda functions in germany.py

--- scrapers/germany.py
+++ scrapers/germany.py
@@ -8,7 +8,6 @@ import aiohttp
 from loguru import logger as log
 from parsel import Selector
 from typing_extensions import TypedDict
-
 from yellowpages.proxy import Proxy
 from yellowpages.utils import EventManager, make_request
 
@@ -61,7 +60,7 @@ def parse_company(company_info) -> Company:
         # Loop through each pair of characters after the key
         for i in range(2, len(encoded), 2):
             # Convert the pair from hex to int, XOR with the key, and get the character
-            char_code = int(encoded[i : i + 2], 16) ^ key
+            char_code = int(encoded[i : i + 2], 16) ^ key  # noqa: E203
             decoded_email_chars.append(chr(char_code))
 
         # Join the characters to get the decoded email
@@ -79,8 +78,8 @@ def parse_company(company_info) -> Company:
 
     selector = Selector(text=company_info)
 
-    first = lambda css: selector.css(css).get("").strip()
-    many = lambda css: ", ".join(
+    first = lambda css: selector.css(css).get("").strip()  # noqa: E731
+    many = lambda css: ", ".join(  # noqa: E731
         set([value.strip() for value in selector.css(css).getall()])
     )
 
@@ -113,7 +112,7 @@ def parse_search(response) -> Preview:
 
     for result in sel.css("div.entry"):
         try:
-            first = lambda css: result.css(css).get("").strip()
+            first = lambda css: result.css(css).get("").strip()  # noqa: E731
             name = first("span[itemprop='name']::text")
             if not name:
                 continue
